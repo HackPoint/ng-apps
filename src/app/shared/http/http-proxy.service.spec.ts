e21bcf2da2d1b2508f229225a9c9369303e90484
hackp0int <genashm@gmail.com>
2017-11-09 18:55:30 +0200

    +adding http-proxy-client

--- src/app/shared/http/http-proxy.service.spec.ts
+++ src/app/shared/http/http-proxy.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+// import { async, fakeAsync, tick } from '@angular/core/testing';
 
 import {
   HttpClientTestingModule,
@@ -10,7 +11,7 @@ import { HttpProxyService } from './http-proxy.service';
 
 const BASE_URL = '';
 
-describe('HttpProxyService', () => {
+describe('HttpProxyService', async () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule, HttpClientTestingModule],
@@ -22,18 +23,22 @@ describe('HttpProxyService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should get users', inject(
+  it('should GET data', inject(
     [HttpTestingController, HttpProxyService],
     (httpMock: HttpTestingController, proxy: HttpProxyService) => {
-      const userUrl: string = `${BASE_URL}/questions`;
-      proxy.get(`${BASE_URL}/questions`).subscribe((next) => {
-        expect(next).toEqual({ name: 'Gena' });
+      const userUrl = `${BASE_URL}/data`;
+      proxy.get(`${BASE_URL}/data`).subscribe((next) => {
+        expect(next).toEqual({name: 'Gena'});
       });
-
       httpMock.match({
         url: userUrl,
         method: 'GET'
-      })[0].flush({ name: 'Gena' });
+      })[0].flush({name: 'Gena'});
+      httpMock.verify();
     })
   );
+
+  it('should POST send data to server', () => {
+
+  });
 });
--- src/app/shared/http/http-proxy.service.ts
+++ src/app/shared/http/http-proxy.service.ts
@@ -4,7 +4,6 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable()
 export class HttpProxyService {
-
   constructor(private http: HttpClient) {
   }
 
