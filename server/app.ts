5d37ded669766a103028a11d4f48753df1b73d09
hackp0int <genashm@gmail.com>
2017-11-13 12:40:00 +0200

    +fixing server; still have an issue with .env file

--- server/app.ts
+++ server/app.ts
@@ -8,9 +8,11 @@ import { ApiRoutes } from './routes/api';
 
 const app = express();
 const apiRoutes = new ApiRoutes();
-dotenv.config({path: '.env'});
 
+process.env.NODE_ENV = 'dev';
+dotenv.config({path: '.env'});
 app.set('port', (process.env.PORT || 3000));
+
 app.use('/', express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,7 +29,7 @@ if (process.env.NODE_ENV === 'test') {
 app.use((req, res, next) => {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -45,31 +47,5 @@ app.use((req, res, next) => {
 
 
 apiRoutes.init(app);
-
-/*mongoose.Promise = global.Promise;
-const mongodb = mongoose.connect(mongodbURI, { useMongoClient: true });
-
-mongodb
-  .then((db) => {
-    console.log('Connected to MongoDB on', db.host + ':' + db.port);
-
-    setRoutes(app);
-
-    app.get('/*', function(req, res) {
-      res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-
-    if (!module.parent) {
-      app.listen(app.get('port'), () => {
-        console.log('Angular Full Stack listening on port ' + app.get('port'));
-      });
-    }
-
-  })
-  .catch((err) => {
-    console.error(err);
-});
-
-*/
 
 export { app };
